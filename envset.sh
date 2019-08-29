238900fbf837e94e80cb2257704f4da077cb4f63
TPFRL <tbvj5914@naver.com>
2019-08-29 15:21:27 +0900

    moved sshd to common config

--- envset.sh
+++ envset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#install 
+#install tmux, etc.
 apt-get install -y tmux silversearcher-ag ctags
 
 #install fzf
@@ -25,22 +25,19 @@ apt-get update
 apt-get install -y neovim
 pip install neovim
 
-#sshd
-apt-get install -y openssh-server net-tools
+#sshd config. assume sshd is already made.
 sed -ri 's/#Port 22/Port 7777/g' /etc/ssh/sshd_config
-service ssh start
 
 #envset
-git clone https://github.com/TPFRL/envset.git
-cp envset/.tmux.conf ~/.tmux.conf
-cp -r envset/.config ~
+cp .tmux.conf ~/.tmux.conf
+cp -r .config ~
 
+#Vim Plugins
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 nvim +'PlugInstall --sync' +qa
 
+#zshrc settings
 echo "alias confloc='./configure --prefix=${HOME}/.local'" >> ${ZDOTDIR:-$HOME}/.zshrc
-
 echo "alias ca='conda activate'"  >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "bindkey '^[[C' emacs-forward-word"  >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "bindkey '^[[D' emacs-backward-word"  >> ${ZDOTDIR:-$HOME}/.zshrc
