5db5090fa6ba79272bbf17553cb69d2a6e982ef7
TPFRL <TPFRL@github.com>
2019-11-06 10:50:59 +0900

    minimum settings

---
+++ minimum/en.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#Vim Plugins
+wget --force-directories -O ~/.vim/autoload/plug.vim \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
---
+++ minimum/envset.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#install tmux, etc.
+apt-get install -y tmux ctags
+
+#envset
+cp .tmux.conf ~/.tmux.conf
+cp .vimrc ~/.vimrc
+
+#Vim Plugins
+mkdir -p ~/.vim/autoload
+wget -O ~/.vim/autoload/plug.vim \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +'PlugInstall --sync' +qa
+
+#bashrc settings
+echo "alias confloc='./configure --prefix=${HOME}/.local'" >> ~/.bashrc
+echo "alias ca='conda activate'"  >> ~/.bashrc
+echo "bindkey '^[[C' emacs-forward-word"  >> ~/.bashrc
+echo "bindkey '^[[D' emacs-backward-word"  >> ~/.bashrc
+echo "bindkey '^W' backward-kill-word"  >> ~/.bashrc
