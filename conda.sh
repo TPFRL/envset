9ee8556e1d850f7eee7b782bca9a93c541bc9b90
TPFRL <tbvj5914@naver.com>
2020-04-18 22:19:30 +0900

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -16,7 +16,7 @@ pip uninstall -y apex || :
 cd /tmp/unique_for_apex/apex && pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
 
 ########
-ARG CONDA_NAME=torch101
+export CONDA_NAME=torch101
 conda create -n ${CONDA_NAME} pip python=3.6 && conda activate ${CONDA_NAME}
 conda install pytorch==1.0.1 torchvision==0.2.2 cudatoolkit=10.0 -c pytorch
 
@@ -26,10 +26,10 @@ pip uninstall -y apex || :
 cd /tmp/unique_for_apex/apex && pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
 
 ########
-ARG CONDA_NAME=py2tf113
+export CONDA_NAME=py2tf113
 conda create -n ${CONDA_NAME} pip python=2.7 && conda activate ${CONDA_NAME}
 pip install tensorflow-gpu==1.13.2
 
-ARG CONDA_NAME=tf113
+export CONDA_NAME=tf113
 conda create -n ${CONDA_NAME} pip python=3.6 && conda activate ${CONDA_NAME}
 pip install tensorflow-gpu==1.13.2
