6840a2feb2352c0a0f3579e30819e43008b8c1bd
TPFRL <tbvj5914@naver.com>
2021-05-26 17:23:55 +0900

    Update envset.sh

--- envset.sh
+++ envset.sh
@@ -35,7 +35,7 @@ cp .tmux.conf ~/
 apt-get install -y locales && \
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
-echo "export PATH="/usr/local/cuda/bin":$PATH" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "export PATH=/usr/local/cuda/bin:$PATH" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "export LC_ALL=en_US.UTF-8" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "export LC_CTYPE=en_US.UTF-8" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "export LANG=en_US.UTF-8" >> ${ZDOTDIR:-$HOME}/.zshrc
