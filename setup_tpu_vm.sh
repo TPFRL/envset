0714c28030ccb991afae406c23a60b13c38818f9
TPFRL <tbvj5914@naver.com>
2021-09-15 00:18:16 +0900

    Update setup_tpu_vm.sh

--- setup_tpu_vm.sh
+++ setup_tpu_vm.sh
@@ -4,4 +4,4 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 chmod +x envset_sudo.sh && ./envset_sudo.sh
 
 pip install crcmod
-export PATH=$HOME/.local/bin:$PATH >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "export PATH=$HOME/.local/bin:$PATH" >> ${ZDOTDIR:-$HOME}/.zshrc
